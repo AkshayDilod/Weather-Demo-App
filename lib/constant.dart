f31a4147e8ddf72e247c4367891e009cb4058434
Akshay <akshaydilod@live.com>
2021-09-21 10:20:48 +0530

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 const white = Colors.white;
 const listTileColor = Color(0xffebeffb);
-const apiKey = '281f93875707272768c9919146ce71f3';
 
 const cloud = 'assets/img/cloud.png';
 const bg = 'assets/img/background.jpg';
